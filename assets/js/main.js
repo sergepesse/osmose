55a45a2d7c125ed6423affea0db9a4cadea1c75c
Serge Pesse <serge.pesse@unige.ch>
2026-07-15 16:48:40 +0200

    btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,10 @@ const bottlesContainer = document.querySelector("#liens");
 
 function createBottleCard(bottle) {
   return `
-    <div class="bottle-card">
+    <div class="bottle-card d-flex flex-column align-items-center my-4">
       <h2>${bottle.name}</h2>
       <a href="boissons.html?id=${bottle.id}"><img src="${bottle.image}" class="w-100" /></a>
+      <button data-id="${bottle.id}" class="btn btn-primary mt-auto btn-bottle">Voir les détails</button>
     </div>
   `;
 }
@@ -14,7 +15,6 @@ function createBottleCard(bottle) {
 async function displayBottles() {
   try {
     const bottles = await getBottles();
-    console.log("Bouteilles récupérées :", bottles);
     bottlesContainer.innerHTML = bottles.map(createBottleCard).join("");
   } catch (error) {
     console.error("Erreur lors de l'affichage des bouteilles :", error);
@@ -22,3 +22,10 @@ async function displayBottles() {
 }
 
 displayBottles();
+
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("btn-bottle")) {
+    const bottleId = event.target.getAttribute("data-id");
+    window.location.href = `boissons.html?id=${bottleId}`;
+  }
+});
